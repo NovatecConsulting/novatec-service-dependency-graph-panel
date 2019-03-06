c3bbdb49e1cd825b207dff2b136244cda0d80512
Marius Oehler <marius.oehler@novatec-gmbh.de>
2019-03-06 14:20:06 +0100

    Added babel for JS files

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.tsx?$/,
         loaders: [
           {
